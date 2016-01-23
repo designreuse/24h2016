980e0289e972bb28462b0c396434a9216dd0cea1
Guillaume Louvel <louvel@akelio.fr>
2016-01-23 14:10:33 +0100

    no message

---
+++ src/main/java/com/bee/team/all/Cell.java
@@ -0,0 +1,23 @@
+package com.bee.team.all;
+
+public class Cell {
+	String	type;
+	int		angle;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getAngle() {
+		return angle;
+	}
+
+	public void setAngle(int angle) {
+		this.angle = angle;
+	}
+
+}
