3a69ff9df6df7fdb7b7d4137b190cd923b901e4a
Augustin Delale <adelale@hotmail.com>
2016-01-23 17:47:31 +0100

    no message

--- src/main/java/com/bee/team/all/LaserBuilder.java
+++ src/main/java/com/bee/team/all/LaserBuilder.java
@@ -23,14 +23,22 @@ public class LaserBuilder {
 		Point nextPoint = findNextPoint(point,direction);
 		Cell nextCell = board.getCellFromPoint(nextPoint);
 		String type = nextCell.getType();
-		int nextDirection = findNextDirection(type,direction);
+		int angle = nextCell.getAngle();
 		
+		int nextDirection = findNextDirection(type,direction,angle);
 		
+		path.add(nextPoint);
+		nextCell.setLaserOrigin(nextDirection);
+		
+		if(nextDirection!=Cell.UNDEFINED)
+			handleDirection(board, path, nextPoint, nextDirection);
 	}
 	
 	private Point findNextPoint(Point p, int direction) {
+		
 		int x = p.getX();
 		int y = p.getY();
+		
 		switch(direction) {
 		case Cell.N:x--;break;
 		case Cell.S:x++;break;
@@ -41,13 +49,45 @@ public class LaserBuilder {
 	}
 	
 	
-	private int findNextDirection(String type, int direction) {
+	private int findNextDirection(String type, int direction, int angle) {
+		
 		if(type.equals(Cell.CELL_EMPTY)) return direction;
-		if(type.equals(Cell.CELL_LASER_START)) return -1;
-		if(type.equals(Cell.CELL_LASER_END)) return -1;
-		if(type.equals(Cell.CELL_WALL)) return -1;
-		if(type.equals(Cell.CELL_MIRROR)) return -1;
-		return 0;
+		if(type.equals(Cell.CELL_LASER_START)) return Cell.UNDEFINED;
+		if(type.equals(Cell.CELL_LASER_END)) return Cell.UNDEFINED;
+		if(type.equals(Cell.CELL_WALL)) return Cell.UNDEFINED;
+		if(type.equals(Cell.CELL_MIRROR)) return reflectOnMirror(direction,angle);
+		
+		return Cell.UNDEFINED;
+	}
+	
+	private int reflectOnMirror(int direction, int angle) {
+		
+		if(angle==Cell.N) {
+			if(direction==Cell.N) return Cell.UNDEFINED;
+			if(direction==Cell.E) return Cell.UNDEFINED;
+			if(direction==Cell.S) return Cell.E;
+			if(direction==Cell.W) return Cell.N;
+		}
+		if(angle==Cell.E) {
+			if(direction==Cell.N) return Cell.W;
+			if(direction==Cell.E) return Cell.UNDEFINED;
+			if(direction==Cell.S) return Cell.UNDEFINED;
+			if(direction==Cell.W) return Cell.S;
+		}
+		if(angle==Cell.S) {
+			if(direction==Cell.N) return Cell.W;
+			if(direction==Cell.E) return Cell.S;
+			if(direction==Cell.S) return Cell.UNDEFINED;
+			if(direction==Cell.W) return Cell.UNDEFINED;
+		}
+		if(angle==Cell.W) {
+			if(direction==Cell.N) return Cell.UNDEFINED;
+			if(direction==Cell.E) return Cell.N;
+			if(direction==Cell.S) return Cell.W;
+			if(direction==Cell.W) return Cell.UNDEFINED;
+		}
+		
+		return Cell.UNDEFINED;
 	}
 	
 }
