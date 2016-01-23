c5fb77e71aff95aae7861b0401dbd1b27f5a3413
Augustin Delale <adelale@hotmail.com>
2016-01-23 16:22:03 +0100

    no message

--- src/main/java/com/bee/team/all/Laser.java
+++ src/main/java/com/bee/team/all/Laser.java
@@ -4,37 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Laser {
-	List<Point>	laser	= new ArrayList<Point>();
-	Point start;
-	Point end;
 	
-	public Laser(Point p) {
-		this.laser = new ArrayList<Point>();
-		this.laser.add(p);
+	private List<Point>	path;
+	
+	private int startDirection;
+	private Point start;
+	private Point end;
+	
+	public Laser(Point start, Point end, int startDirection) {
+		
+		this.start = start;
+		this.end = end;
+		this.startDirection = startDirection;
+		this.path = new ArrayList<>();
 	}
 
-	public List<Point> getLaser() {
-		return laser;
-	}
-
-	public void setLaser(List<Point> laser) {
-		this.laser = laser;
+	public List<Point> getPath() {
+		return path;
 	}
 
 	public Point getStart() {
 		return start;
 	}
 
-	public void setStart(Point start) {
-		this.start = start;
-	}
-
 	public Point getEnd() {
 		return end;
 	}
-
-	public void setEnd(Point end) {
-		this.end = end;
+	
+	public int getStartDirection() {
+		return startDirection;
 	}
-
 }
--- src/main/java/com/bee/team/all/LaserBuilder.java
+++ src/main/java/com/bee/team/all/LaserBuilder.java
@@ -1,20 +1,33 @@
 package com.bee.team.all;
 
+import java.util.List;
+
 public class LaserBuilder {
 	
 
 	
 	public void compute(Laser laser, Board board) {
 		
-		laser.reset();
+		List<Point> path = laser.getPath();
 		Point start = laser.getStart();
-		int direction = findStartDirection(start);
+		int direction = laser.getStartDirection();
+		
+		path.clear();
+		
+		handleDirection(board, path,start,direction);
+	}
+	
+	
+	private void handleDirection(Board board, List<Point> path, Point p, int direction) {
+		Cell currentCell = getCellFromPoint(board,p);
+		
+		String currentCellType = currentCell.getType();
 		
 		
 	}
 	
 	
-	private int findStartDirection(Point p) {
-		return Cell.N;
+	private Cell getCellFromPoint(Board board, Point p) {
+		return board.getCells()[p.getX()][p.getY()];
 	}
 }
