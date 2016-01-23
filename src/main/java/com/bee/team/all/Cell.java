89871acc8a848d6a8c6f338add855693ab9f9bea
Guillaume Louvel <louvel@akelio.fr>
2016-01-23 15:20:49 +0100

    no message

--- src/main/java/com/bee/team/all/Cell.java
+++ src/main/java/com/bee/team/all/Cell.java
@@ -1,8 +1,25 @@
 package com.bee.team.all;
 
 public class Cell {
-	String	type;
-	int		angle;
+	static String	CELL_EMPTY			= "empty";
+	static String	CELL_WALL			= "wall";
+	static String	CELL_MIROR			= "miror";
+	static String	CELL_LASER_START	= "lstart";
+	static String	CELL_LASER_END		= "lend";
+
+	String			type;
+	int				angle;
+
+	public Cell(String type) {
+		super();
+		this.type = type;
+	}
+
+	public Cell(String type, int angle) {
+		super();
+		this.type = type;
+		this.angle = angle;
+	}
 
 	public String getType() {
 		return type;
